814b21695e7e1b70116520d5fcfde20863ff82ab
Ivan Krutov <vania-pooh@vania-pooh.com>
2016-04-03 19:11:01 +0300

    Fixed Openstack files

--- openstack/entry_point.sh
+++ openstack/entry_point.sh
@@ -2,26 +2,30 @@
 
 CLOUDS_XML="/etc/perspective/openstack/clouds.xml"
 
-if [ -z "$NAME" ]; then
+if [ -n "$NAME" ]; then
     sed -i "s|cloud_id|$NAME|g" $CLOUDS_XML
     sed -i "s|cloud_name|$NAME|g" $CLOUDS_XML
 fi
 
-if [ -z "$ENDPOINT" ]; then
+if [ -n "$ENDPOINT" ]; then
     sed -i "s|http://example.com/|$ENDPOINT|g" $CLOUDS_XML
 fi
 
-if [ -z "$PROJECT_NAME" -a -z "$LOGIN" ]; then
+if [ -n "$PROJECT_NAME" -a -n "$LOGIN" ]; then
     sed -i "s|username|$PROJECT_NAME:$LOGIN|g" $CLOUDS_XML
 fi
 
-if [ -z "$PASSWORD" ]; then
+if [ -n "$PASSWORD" ]; then
     sed -i "s|password|$PASSWORD|g" $CLOUDS_XML
 fi
 
-sudo restart perspective-storage
+GC_OPTS="-XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"
+MEMORY_OPTS="-Xmx512m"
+JAVA_ARGS="$GC_OPTS $MEMORY_OPTS -Djava.net.preferIPv6Addresses=true"
+
+java $JAVA_ARGS -Xbootclasspath/a:/etc/perspective/storage -jar /usr/share/perspective/perspective-storage/perspective-storage.jar >> /var/log/perspective/perspective-storage.log 2>&1 &
 sleep 0.5
-sudo restart perspective-rest
-sudo restart perspective-openstack
+java $JAVA_ARGS -jar /usr/share/perspective/perspective-rest/jetty-runner.jar --classes /etc/perspective /usr/share/perspective/perspective-rest/perspective-rest.war >> /var/log/perspective/perspective-rest.log 2>&1 &
+java $JAVA_ARGS -jar /usr/share/perspective/perspective-openstack/perspective-openstack.jar >> /var/log/perspective/perspective-openstack.log 2>&1 &
 
 perspective
